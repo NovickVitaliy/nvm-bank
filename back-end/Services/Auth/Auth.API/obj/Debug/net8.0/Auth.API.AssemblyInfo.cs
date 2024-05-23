0a4c527657004be5de265954a2fe4bf827b1e5d4
Vitalii Novik <novickvitaliy@gmail.com>
2024-05-23 23:28:40 +0300

    deploy Auth.API to docker

--- back-end/Services/Auth/Auth.API/obj/Debug/net8.0/Auth.API.AssemblyInfo.cs
+++ back-end/Services/Auth/Auth.API/obj/Debug/net8.0/Auth.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Auth.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8454edaed545a63e279b373c6f6cf3ef07975371")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d49a6fbfc1f56c40a975c9970415dd6fc62d598")]
 [assembly: System.Reflection.AssemblyProductAttribute("Auth.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Auth.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
