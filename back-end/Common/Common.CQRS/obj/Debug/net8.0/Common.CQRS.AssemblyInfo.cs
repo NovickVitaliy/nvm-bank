82dc0ddc36a3b5e194014505f9d09c2a4bd5c35a
Vitalii Novik <novickvitaliy@gmail.com>
2024-05-23 13:33:57 +0300

    internal files changes

--- back-end/Common/Common.CQRS/obj/Debug/net8.0/Common.CQRS.AssemblyInfo.cs
+++ back-end/Common/Common.CQRS/obj/Debug/net8.0/Common.CQRS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Common.CQRS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6568d06860342e19d56094aa3aec4cd26c8fbc9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+686b40bfa60c740912f8c1ffba122655e5eab992")]
 [assembly: System.Reflection.AssemblyProductAttribute("Common.CQRS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Common.CQRS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
