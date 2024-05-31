cec8f878c8a434d8bff64e7aa6270518d4e8539c
Vitalii Novik <novickvitaliy@gmail.com>
2024-05-31 16:44:05 +0300

    configure common services in checkings.api

--- back-end/Services/Checkings/Checkings.API/Program.cs
+++ back-end/Services/Checkings/Checkings.API/Program.cs
@@ -1,15 +1,51 @@
+using System.Reflection;
+using Carter;
 using Checkings.API.Data;
+using Common.Auth;
+using Common.CQRS.Behaviours;
+using Common.ErrorHandling;
+using Common.Logging;
+using Common.Messaging.Extension;
+using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.ConfigureLogging();
+
+builder.Services.ConfigureAuthentication(builder.Configuration);
+
+var assembly = Assembly.GetExecutingAssembly();
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddMediatR(config =>
+{
+    config.AddOpenBehavior(typeof(LoggingBehaviour<,>));
+    config.AddOpenBehavior(typeof(ValidationBehaviour<,>));
+    config.RegisterServicesFromAssembly(assembly);
+});
+
+builder.Services.AddValidatorsFromAssembly(assembly);
+
+builder.Services.ConfigureMessageBroker(builder.Configuration);
+
+builder.Services.AddCarter();
 
 builder.Services.AddDbContext<CheckingsDbContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("CheckingsDb"));
 });
 
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseExceptionHandler(options => { });
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapCarter();
 
 app.Run();
