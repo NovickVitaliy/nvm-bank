98e84438a05c26098bdc496fc0f65a0aae9e1d55
Vitalii Novik <novickvitaliy@gmail.com>
2024-06-11 16:41:02 +0300

    change IEnumerable<> to IReadOnlyList<> in GetTransactionsByAccount() method of transactions repo

--- back-end/Services/Transaction/Transaction.Application/Data/ITransactionRepository.cs
+++ back-end/Services/Transaction/Transaction.Application/Data/ITransactionRepository.cs
@@ -7,5 +7,5 @@ public interface ITransactionRepository
 {
     Task<Result<Guid>> CreateTransaction(CreateTransactionDto createTransactionDto);
     Task<Result<TransactionDto>> GetTransaction(Guid id);
-    Task<Result<IEnumerable<TransactionDto>>> GetTransactionsByAccount(Guid accountNumber);
+    Task<Result<IReadOnlyList<TransactionDto>>> GetTransactionsByAccount(Guid accountNumber);
 }
--- back-end/Services/Transaction/Transaction.Infrastructure/Repositories/TransactionRepository.cs
+++ back-end/Services/Transaction/Transaction.Infrastructure/Repositories/TransactionRepository.cs
@@ -10,24 +10,21 @@ using Transaction.Infrastructure.Options.Mongo;
 
 namespace Transaction.Infrastructure.Repositories;
 
-public class TransactionRepository : ITransactionRepository
-{
+public class TransactionRepository : ITransactionRepository {
     private readonly IMongoClient _mongoClient;
     private readonly MongoOptions _mongoOptions;
     private readonly IMongoCollection<Domain.Models.Transaction> _transactions;
 
     public TransactionRepository(
         IMongoClient mongoClient,
-        IOptions<MongoOptions> options)
-    {
+        IOptions<MongoOptions> options) {
         _mongoClient = mongoClient;
         _mongoOptions = options.Value;
         _transactions = _mongoClient.GetDatabase(_mongoOptions.DatabaseName)
             .GetCollection<Domain.Models.Transaction>(_mongoOptions.TransactionsCollectionName);
     }
 
-    public async Task<Result<Guid>> CreateTransaction(CreateTransactionDto createTransactionDto)
-    {
+    public async Task<Result<Guid>> CreateTransaction(CreateTransactionDto createTransactionDto) {
         var transaction = Domain.Models.Transaction.Create(
             new AccountNumber(createTransactionDto.Source),
             new AccountType(createTransactionDto.SourceType),
@@ -42,25 +39,23 @@ public class TransactionRepository : ITransactionRepository
         return Result<Guid>.Success(transaction.Id.Value);
     }
 
-    public async Task<Result<TransactionDto>> GetTransaction(Guid id)
-    {
+    public async Task<Result<TransactionDto>> GetTransaction(Guid id) {
         var transaction = await (await _transactions
                 .FindAsync(x => x.Id.Equals(new TransactionId(id))))
             .FirstOrDefaultAsync();
 
-        if (transaction is null)
-        {
+        if (transaction is null) {
             //TODO: return result response
         }
 
         return Result<TransactionDto>.Success(transaction.ToTransactionDto());
     }
 
-    public async Task<Result<IEnumerable<TransactionDto>>> GetTransactionsByAccount(Guid accountNumber)
-    {
+    public async Task<Result<IReadOnlyList<TransactionDto>>> GetTransactionsByAccount(Guid accountNumber) {
         var transactions = await (await _transactions.FindAsync(x => x.Source.Equals(new AccountNumber(accountNumber))))
             .ToListAsync();
 
-        return Result<IEnumerable<TransactionDto>>.Success(transactions.Select(x => x.ToTransactionDto()));
+        return Result<IReadOnlyList<TransactionDto>>.Success(transactions.Select(x => x.ToTransactionDto()).ToList()
+            .AsReadOnly());
     }
 }
