449e47eb9bc6b7d688233fc34a9b3230f6f2ca81
Vitalii Novik <novickvitaliy@gmail.com>
2024-06-11 18:42:05 +0300

    configure dependencies at transactions api

--- back-end/Services/Transaction/Transaction.API/Program.cs
+++ back-end/Services/Transaction/Transaction.API/Program.cs
@@ -2,8 +2,10 @@ using System.Reflection;
 using Common.Auth;
 using Common.CQRS.Behaviours;
 using Common.Logging;
+using Common.Messaging.Extension;
 using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
+using Transaction.Infrastructure;
 
 [assembly:ApiController]
 
@@ -11,9 +13,41 @@ var assembly = Assembly.GetExecutingAssembly();
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers()
+    .ConfigureApiBehaviorOptions(options => {
+
+        var builtInFactory = options.InvalidModelStateResponseFactory;
+
+        options.InvalidModelStateResponseFactory = context => {
+            var logger = context.HttpContext.RequestServices.GetRequiredService<ILogger<Program>>();
+
+            logger.LogError("Invalid model state at route {Route}", context.HttpContext.Request.Path.ToString());
+
+            return builtInFactory(context);
+        };
+    });
+
+builder.ConfigureLogging();
+
+builder.Services.AddMediatR(config => {
+    config.RegisterServicesFromAssembly(assembly);
+
+    config.AddOpenBehavior(typeof(LoggingBehaviour<,>));
+    config.AddOpenBehavior(typeof(ValidationBehaviour<,>));
+});
+
+builder.Services.AddValidatorsFromAssembly(assembly);
+
+builder.Services.ConfigureAuthentication(builder.Configuration);
+
+builder.Services.ConfigureInfrastructure(builder.Configuration);
+
+builder.Services.ConfigureMessageBroker(builder.Configuration);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
